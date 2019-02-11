e877eb15dec65d93242007e7938a0f647c3d0cae
JK <jk.jonggyuk@gmail.com>
2019-02-10 23:31:43 -0800

    commit

--- Textifier/app/src/main/java/com/jjk/textifier/MainActivity.java
+++ Textifier/app/src/main/java/com/jjk/textifier/MainActivity.java
@@ -2,6 +2,9 @@ package com.jjk.textifier;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCamera = (Button)findViewById(R.id.btnCamera);
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            protected Object clone() throws CloneNotSupportedException {
+                return super.clone();
+            }
+        });
     }
 }
